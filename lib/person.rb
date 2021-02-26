cd70662e518e2df84388e808501e348000aad64f
Posyv <nataliepvogt@gmail.com>
2021-02-25 21:17:10 -0500

    commit tree eeaf9353ded60532af681002c117b93dcbb43052

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :happiness, :hygiene, :money
+
+    def initialize NewPerson(name, happiness=8, hygiene=8, money=25)
+        @name = name
+        @happiness = happiness
+        @hygiene = hygiene
+        @money = money
+    end
+
+    def contingency
+        if @happiness >= 7
+            return true
+        else return false 
+        end
+
+end
+
